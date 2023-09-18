13df1841ad4b49d6e8cad0d639b0ef939f80d5e4
bdlc123 <bryant.delacruz54@gmail.com>
2023-09-17 20:50:20 -0400

    registering on admin

--- orderup/admin.py
+++ orderup/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order,Customer,Item
+
+admin.site.register(Order)
+admin.site.register(Item)
+admin.site.register(Customer)
 
 # Register your models here.
